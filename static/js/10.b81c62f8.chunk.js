b0f30b3a1b9cdb8652a2aae4cb3778a7f1452d75
smoreweb <jianjun@Lins-MacBook-Pro.local>
2023-04-06 14:31:32 +0800

    update

--- static/js/10.b81c62f8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplynk-app"]=this["webpackJsonplynk-app"]||[]).push([[10],{751:function(p,n){}}]);
-//# sourceMappingURL=10.b81c62f8.chunk.js.map
--- static/js/8.0cebdc3a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplynk-app"]=this["webpackJsonplynk-app"]||[]).push([[8],{788:function(n,p){},805:function(n,p){},811:function(n,p){},813:function(n,p){}}]);
-//# sourceMappingURL=8.0cebdc3a.chunk.js.map
